3f47c0e4dc4dc216f5f7c94a0089e88fdcc5d165
IliyaSavin <49229895+IliyaSavin@users.noreply.github.com>
2021-10-16 01:58:31 +0300

    get login

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,7 @@ module.exports = function(req, res, next) {
     try {
         const decoded = jwt.verify(token, config.get('jwtSecret'));
 
-        req.user = decoded.user;
+        req.user = decoded.client;
         next();
     } catch (err) {
         res.json({ msg: 'Token is not valid'});
